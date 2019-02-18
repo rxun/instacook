387a7a139efd480a8242a47a2db3fb59d172bc6b
Glitch (hello-webpage) <none>
2019-02-18 10:07:44 +0000

    💣💔 Checkpoint ./.start.sh:1842442/697

--- .start.sh
+++ .start.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# I really like how 
-# copied from https://glitch.com/~python3
+# my original naive approach 
+pip3 install --user -r .requirements.txt
+python3 backend/main.py
+
+# 
+# ...check out https://glitch.com/~python3 for nicer approach
 #
-MD5=$(md5sum .requirements.txt | cut -f1 -d' ')
-if ! [ -d ".local/lib/python3/$MD5-site-packages" ]; then
-    rm -rf .local/lib/python3/*-site-packages
-    pip3 install -U -r .requirements.txt -t ".local/lib/python3/$MD5-site-packages" 
-fi
-exec env PYTHONPATH="$PWD/.local/lib/python3/$MD5-site-packages" python3 backend/main.py
