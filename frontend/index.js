ed07589601a77ef4f5de3f1abae92382c510b6a9
Glitch (hello-webpage) <none>
2021-04-01 19:35:42 +0000

    Exporting to rxun/instacook ./frontend/index.js:5042597/54:30472909/27:30472974/218

--- frontend/index.js
+++ frontend/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 
 const App = () => (
   <h1>Helloo</h1>
-);
+); // vibes
 
-<button onClick={}>
-</button>
+function AddRow() {
+  function handleClick(e) {
+    e.preventDefault();
+    console.log('Add row.');
+  }
+  <button onClick={handleClick}>
+    Add Row
+  </button>
+}
 
 ReactDOM.render(<App/>, document.getElementById('app'));
