47428c01a44b5d55ea728591af5bb410dc074dee
Glitch (cs-411) <none>
2021-04-01 19:46:47 +0000

    Format

--- frontend/index.js
+++ frontend/index.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const App = () => (
-  <h1>Helloo</h1>
-); // vibes
+const App = () => <h1>Helloo</h1>; // vibes
 
 function AddRow() {
   function handleClick(e) {
     e.preventDefault();
-    console.log('Add row.');
+    console.log("Add row.");
   }
-  
-  return (<button onClick={handleClick}>
-    Add Row
-  </button>)
+
+  return <button onClick={handleClick}>Add Row</button>;
 }
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
