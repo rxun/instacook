fecac887ba0c986fa47588623fab348d6ba7bd26
Rebecca <rebeccaxun@gmail.com>
2021-04-12 16:45:37 -0500

    deleted header

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 
 const API_URL = "http://localhost:5000/api";
-<<<<<<< HEAD
-
-=======
->>>>>>> main
 const instance = axios.create({
   baseURL: "http://localhost:5000/api",
 });
