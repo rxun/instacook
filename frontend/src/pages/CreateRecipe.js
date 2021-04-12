863ed0dbd63d1544473c2c002b18344b62b2ef62
kenetec <klhester3@gmail.com>
2021-04-12 10:58:54 -0500

    Add options for form

--- frontend/src/pages/CreateRecipe.js
+++ frontend/src/pages/CreateRecipe.js
@@ -1,15 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router";
-import { Form, Input, Button, Checkbox, Card, Space } from "antd";
+import { Form, Input, Button, Checkbox, Card, Space, Radio } from "antd";
 
 import "../css/login.scss";
 import "../css/createrecipe.scss";
 import { getRecipes } from "../utils/api";
 
+const CRUD_OPTIONS = {
+  CREATE: 1,
+  READ: 2,
+  UPDATE: 3,
+  DELETE: 4,
+};
+
 const CreateRecipe = () => {
   let history = useHistory();
   const [steps, setSteps] = useState("");
+  const [recipeId, setRecipeId] = useState("");
   const [recipes, setRecipes] = useState([]);
+  const [crudOption, setCrudOption] = useState(CRUD_OPTIONS.CREATE);
 
   useEffect(() => {
     async function fetchRecipes() {
@@ -28,14 +37,35 @@ const CreateRecipe = () => {
     <div className="create-account-container">
       <h1>InstaCook</h1>
       <Form onFinish={onSubmit}>
-        <Form.Item label="Steps">
-          <Input.TextArea
-            rows={12}
-            onChange={(e) => setSteps(e.target.value)}
-          />
-        </Form.Item>
+        {crudOption !== CRUD_OPTIONS.CREATE && (
+          <Form.Item label="Recipe ID">
+            <Input
+              rows={12}
+              placeholder="0"
+              onChange={(e) => setRecipeId(e.target.value)}
+            />
+          </Form.Item>
+        )}
+        {crudOption !== CRUD_OPTIONS.DELETE &&
+          crudOption !== CRUD_OPTIONS.READ && (
+            <Form.Item label="Steps">
+              <Input.TextArea
+                rows={12}
+                onChange={(e) => setSteps(e.target.value)}
+              />
+            </Form.Item>
+          )}
+        <Radio.Group
+          defaultValue={CRUD_OPTIONS.CREATE}
+          onChange={(e) => setCrudOption(e.target.value)}
+        >
+          <Radio value={CRUD_OPTIONS.CREATE}>Create</Radio>
+          <Radio value={CRUD_OPTIONS.READ}>Read</Radio>
+          <Radio value={CRUD_OPTIONS.UPDATE}>Update</Radio>
+          <Radio value={CRUD_OPTIONS.DELETE}>Delete</Radio>
+        </Radio.Group>
         <Form.Item>
-          <input type="submit" value="Create Recipe" />
+          <input type="submit" value="Run Action" />
         </Form.Item>
       </Form>
 
@@ -43,7 +73,10 @@ const CreateRecipe = () => {
       <div className="recipe-list">
         {recipes &&
           recipes.map((recipe) => (
-            <Card title={`Recipe ${recipe.recipe_id}`} style={{ width: 300, paddingLeft: '1em', paddingRight: '1em' }}>
+            <Card
+              title={`Recipe ${recipe.recipe_id}`}
+              style={{ width: 300, paddingLeft: "1em", paddingRight: "1em" }}
+            >
               <p>{recipe.steps.substring(0, 100)}</p>
             </Card>
           ))}
