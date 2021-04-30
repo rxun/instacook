cfe350ae077e0f1222b53e205399700053e78163
kenetec <klhester3@gmail.com>
2021-04-30 14:05:54 -0500

    Edit num comments

--- backend/src/api/post.py
+++ backend/src/api/post.py
@@ -182,9 +182,10 @@ def get_comments_from_post_id(id):
 
     if numeric:
         query_results = conn.execute(f'''
-        SELECT COUNT(*) as count
+        SELECT P.post_id, COUNT(*) as count
         FROM Comment C NATURAL JOIN Post P
-        WHERE P.post_id = {id};
+        WHERE P.post_id = {id}
+        GROUP BY P.post_id;
         ''')
     else:
         query_results = conn.execute(f'''
